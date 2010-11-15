df797ac4813dc9f7bb35b40d6a88c6e23828dcb1
SamTN <saotoimaileloi@yahoo.com>
2010-11-15 15:11:10 +0700

    Remove count method on the model.

--- lib/fcg_service_clients/models/comment.rb
+++ lib/fcg_service_clients/models/comment.rb
@@ -4,20 +4,7 @@ module FCG
       ATTRIBUTES = [:site, :record, :body, :body_as_html, :deleted, :flagged_by, :depth, :path, :parent_id, :displayed_name, :user_id]
 
       module ClassMethods
-        def count(*args)
-          opts = args.extract_options!
-          request = Typhoeus::Request.new(
-            "#{service_url}/count", :body => hash_to_msgpack(opts),
-            :method => :post)
-          request.on_complete do |response|
-            response
-          end
 
-          self.hydra.queue(request)
-          self.hydra.run
-
-          handle_service_response request.handled_response
-        end
       end
 
       module InstanceMethods
