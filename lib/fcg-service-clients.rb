719fd80c88b29cc85a7ef00852ca03188126e07e
Samuel O. Obukwelu <sam@fcgmedia.com>
2011-03-03 02:18:55 -0500

    we don't require any more models... Everything is autoloaded

--- lib/fcg-service-clients.rb
+++ lib/fcg-service-clients.rb
@@ -1,6 +1,8 @@
 require 'rubygems' 
 require 'active_model'
 require 'active_support'
+require 'active_support/inflector'
+
 require 'typhoeus'
 require 'msgpack'
 require 'hashie'
@@ -8,21 +10,30 @@ require 'facets'
 require 'bunny'
 require 'fcg-service-ext'
 require 'fcg-core-ext'
-# require 'net/http'
-# require 'uri'
 include Hashie::HashExtensions
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-# load all models + client files
+# load all client files
 Dir[
   File.expand_path("../service/client/base.rb", __FILE__),
   File.expand_path("../service/client/configuration.rb", __FILE__),
   File.expand_path("../service/client/sender.rb", __FILE__),
   File.expand_path("../fcg_service_clients/thor/*.rb", __FILE__),
   File.expand_path("../fcg_service_clients/*.rb", __FILE__),
-  File.expand_path("../fcg_service_clients/client/*.rb", __FILE__),
-  File.expand_path("../fcg_service_clients/models/*.rb", __FILE__)
+  File.expand_path("../fcg_service_clients/client/*.rb", __FILE__)
 ].each do |file|
   require file
+end
+
+module FCG
+  module Client
+    # autoload all models
+    Dir[
+      File.expand_path("../fcg_service_clients/models/*.rb", __FILE__)
+    ].each do |file|
+      name = File.basename(file, ".rb")
+      autoload "#{name.classify}".to_sym, file
+    end
+  end
 end
