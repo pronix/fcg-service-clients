86f7b6acc3a1ef82af7d7b34cce3085bff049870
SamTN <saotoimaileloi@yahoo.com>
2011-03-07 19:42:20 +0700

    Rename authentication.rb to authenticator.rb on gemspec file. Add follower mode, add leaders_feed to status model, add followers, followed, unfollow  functions, rename model attributes from leaders -> leader_of, followers to total_followers.

--- lib/fcg_service_clients/models/status.rb
+++ lib/fcg_service_clients/models/status.rb
@@ -4,7 +4,16 @@ module FCG
       ATTRIBUTES = [:created_at, :message, :message_as_html, :updated_at, :user_id, :visible].freeze
 
       module ClassMethods
-        
+        def leaders_status(follower_id, *args)
+          opts = args.extract_options!
+          params = {
+
+            :limit => 10,
+            :skip => 0
+          }.merge(opts)
+          request = send_to_server(:method => :get, :path => "#{service_url}/#{follower_id}/leaders/status", :params => params)
+          handle_service_response request.handled_response
+        end
       end
 
       module InstanceMethods
